cb411e42ecbe2c524027e443ee83102f8e7c1d62
kevin.thizy <kevin.thizy@outlook.fr>
2025-12-07 19:13:40 +0100

    ✨(boosters) Add initial setup for MongoDB integration and define booster and game types

--- app/collection/boosters/page.tsx
+++ app/collection/boosters/page.tsx
@@ -2,7 +2,7 @@ import {getBoosters} from "@/lib/data/boosters";
 
 export default async function BoostersPage() {
   const boosters = await getBoosters({
-    gameId: 'mtg',
+    gameId: '6668cd15056765e4d0fcf113',
   });
 
   console.log(boosters);
