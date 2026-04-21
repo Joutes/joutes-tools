6df30a60f688d78088fe99e3fd89e5c042e21c79
Nakasar <kevin.thizy@outlook.fr>
2026-04-21 13:11:56 +0000

    ✨ Ajouter une entrée pour le site principal dans le fichier sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,6 +3,12 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
+      url: 'https://www.joutes.app',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+    {
       url: 'https://tools.joutes.app',
       lastModified: new Date(),
       changeFrequency: 'yearly',
