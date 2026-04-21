016cb3502191816d98d0a385bfff52ccb52a805f
Nakasar <kevin.thizy@outlook.fr>
2026-04-21 13:11:49 +0000

    ✨ Ajouter un fichier sitemap pour la génération de la carte du site

---
+++ app/sitemap.ts
@@ -0,0 +1,54 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://tools.joutes.app',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://tools.joutes.app/swu',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://tools.joutes.app/riftbound',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://tools.joutes.app/riftbound/erratas',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.6,
+    },
+    {
+      url: 'https://tools.joutes.app/riftbound/cards',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.2,
+    },
+    {
+      url: 'https://tools.joutes.app/riftbound/deck-checker',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.1,
+    },
+    {
+      url: 'https://tools.joutes.app/riftbound/policies',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.5,
+    },
+    {
+      url: 'https://tools.joutes.app/riftbound/game-stats',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.2,
+    },
+  ];
+}
