a90c34805ff56e1f7087515e0a44b7e678c7dbbd
Nakasar <kevin.thizy@outlook.fr>
2026-04-28 09:17:12 +0000

    ✨ Améliorer la gestion des erreurs pour la recherche de cartes et inclure les détails des erratas

--- app/mcp/route.ts
+++ app/mcp/route.ts
@@ -1,16 +1,49 @@
+import { getErratasByCardId } from '@/lib/data/erratas';
+import db from '@/lib/mongodb';
 import { TextContent } from '@modelcontextprotocol/sdk/types.js';
 import { createMcpHandler } from 'mcp-handler';
+import { ObjectId } from 'mongodb';
 import { z } from 'zod/v3';
 
 async function handleSearchCard(params: {
     gameName?: string;
     cardName: string;
 }): Promise<{ content: TextContent[]; isError?: boolean }> {
+    const game = await db.collection("games").findOne({ $or: [{ name: params.gameName }, { slug: params.gameName }] });
+
+    if (!game) {
+        return {
+            content: [
+                {
+                    type: "text",
+                    text: `No game found with name "${params.gameName ?? "N/A"}".`
+                }
+            ],
+            isError: false,
+        };
+    }
+
+    const card = await db.collection("cards").findOne({ name: params.cardName, game: game?._id });
+
+    if (!card) {
+        return {
+            content: [
+                {
+                    type: "text",
+                    text: `No card found with name "${params.cardName}" in game "${params.gameName ?? "N/A"}".`
+                }
+            ],
+            isError: false,
+        };
+    }
+
+    const erratas = await getErratasByCardId(card.id);
+
     return {
       content: [
         {
             type: "text",
-            text: `You searched for card "${params.cardName}" in game "${params.gameName ?? "N/A"}".\n\nThis card has no erratas.`
+            text: `You searched for card "${params.cardName}" in game "${params.gameName ?? "N/A"}".\n\nThis card has ${erratas.length} erratas.\n\nErratas details:\n${erratas.map((e, index) => `\n${index + 1}. Type: ${e.type}, Details: ${e.details}, Source: ${e.source}, Errata ID: ${e.id}`).join("\n")}`
         }
       ],
       isError: false,
