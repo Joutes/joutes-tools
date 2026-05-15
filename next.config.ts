f2039fd3f224be98d808b08a9056fe316a16046d
kevin.thizy <kevin.thizy@outlook.fr>
2026-05-15 17:39:05 +0200

    ✨(auth) Add allowed hosts and protocol configuration for better authentication

--- next.config.ts
+++ next.config.ts
@@ -14,66 +14,8 @@ const nextConfig: NextConfig = {
   },
   experimental: {
     serverActions: {
-      allowedOrigins: process.env.NODE_ENV === "development" ? ["localhost:3000", process.env.DEV_URL ?? "localhost:3000"] : ["tools.joutes.app", "beta.joutes.app", "http://localhost:5173"],
+      allowedOrigins: process.env.NODE_ENV === "development" ? ["localhost:3000", process.env.DEV_URL ?? "localhost:3000"] : undefined,
     }
-  },
-  async headers() {
-    return [
-      {
-        source: "/api/auth/:path*",
-        has: [
-          {
-            type: 'host',
-            value: 'localhost',
-          },
-        ],
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "http://localhost:5173",
-          },
-          {
-            key: "Access-Control-Allow-Credentials",
-            value: "true",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-        ],
-      },
-      {
-        source: "/api/auth/:path*",
-        has: [
-          {
-            type: 'host',
-            value: 'beta.joutes.app',
-          },
-        ],
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://beta.joutes.app",
-          },
-          {
-            key: "Access-Control-Allow-Credentials",
-            value: "true",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-        ],
-      },
-    ];
   },
 };
 
