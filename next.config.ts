49a3b455dc339a936b2b7dd97478a096e459192a
kevin.thizy <kevin.thizy@outlook.fr>
2026-05-15 17:30:42 +0200

    ✨(auth) Add allowed hosts and protocol configuration for better authentication

--- next.config.ts
+++ next.config.ts
@@ -14,7 +14,7 @@ const nextConfig: NextConfig = {
   },
   experimental: {
     serverActions: {
-      allowedOrigins: process.env.NODE_ENV === "development" ? ["localhost:3000", process.env.DEV_URL ?? "localhost:3000"] : ["tools.joutes.app", "beta.joutes.app", "localhost:5173"],
+      allowedOrigins: process.env.NODE_ENV === "development" ? ["localhost:3000", process.env.DEV_URL ?? "localhost:3000"] : ["tools.joutes.app", "beta.joutes.app", "http://localhost:5173"],
     }
   },
   async headers() {
